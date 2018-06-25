81f9a343deb34e82b97ca1b825ac5fc6a195c2ab
shipengqi <pooky.shipengqi@gmail.com>
2018-06-25 18:14:46 +0800

    update cli

--- bin/utils.js
+++ bin/utils.js
@@ -1,6 +1,15 @@
+const path = require('path');
+const prompt = require('prompt');
+const colors = require('colors/safe');
+const {ymlHelper} = require('../lib/utils');
+const commonConfig = path.join(__dirname, '..', 'common.yml');
+
 function create(cmd) {
   console.log(cmd.config);
   console.log(cmd.start);
+  let commonData = ymlHelper.get(commonConfig);
+  console.log(commonData);
+  configConsole({properties: commonData});
 }
 
 function run(cmd) {
@@ -10,6 +19,20 @@ function run(cmd) {
 function createAndRun(cmd) {
   create(cmd);
   run(cmd);
+}
+
+function configConsole(schema) {
+  return new Promise(resolve => {
+    prompt.message = colors.green(">");
+    prompt.delimiter = colors.green(">");
+    prompt.start();
+    prompt.get(schema, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+    });
+  });
 }
 
 module.exports = {
